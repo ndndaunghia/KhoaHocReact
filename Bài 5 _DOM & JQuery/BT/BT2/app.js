9ce24aa0c6af169ad22a5b187fb7e84640002f84
ndndaunghia <ndndaunghia@gmail.com>
2023-03-06 08:26:35 +0700

    done

--- Bài 5 _DOM & JQuery/BT/BT2/app.js
+++ Bài 5 _DOM & JQuery/BT/BT2/app.js
@@ -1,12 +1,22 @@
 const btnCal = document.querySelector(".btn");
 
 btnCal.addEventListener("click", function(){
-    const money = document.getElementById('money');
-    const interest = document.getElementById('interest')
-    const time = document.getElementById('time');
-   const x = Math.pow(money, 1+(interest.value/100));
-   const monthlyPayment = (money.value*x*(interest.value/100))/(x-1);
-    console.log(x);
-    console.log(monthlyPayment)
+    const loan = document.getElementById('loan').value;
+    const interest = document.getElementById('interest').value/100/12;
+    const time = document.getElementById('time').value*12;
 
+    const x = Math.pow(1+interest, time);
+    const monthlyPayment = ((loan*x*interest)/(x-1));
+    const totalToPay = monthlyPayment * time;
+    const totalInterest = totalToPay - loan;
+    console.log(monthlyPayment);
+    console.log(totalToPay);
+    console.log(totalInterest);
+
+    const output_MonthlyPayment = document.getElementById("output1");
+    const output_TotalToPay = document.getElementById("output2");
+    const output_TotalInterest = document.getElementById("output3");
+    output_MonthlyPayment.innerHTML = monthlyPayment.toFixed(2);
+    output_TotalToPay.innerHTML = totalToPay.toFixed(2);
+    output_TotalInterest.innerHTML = totalInterest.toFixed(2);
 })
